608168c73259d9263f8e6c9bc3e69b690a4c27d1
alok-pol <ravikantpal010@gmail.com>
2020-03-22 12:30:14 +0530

    login worked V1.0

--- src/main/java/com/alok/repoph/controller/UtilityUserController.java
+++ src/main/java/com/alok/repoph/controller/UtilityUserController.java
@@ -1,16 +1,9 @@
 package com.alok.repoph.controller;
 
-import com.alok.repoph.common.utils.CommonHelperFunctions;
-import com.alok.repoph.common.utils.Constants;
-import com.alok.repoph.models.UtilityUser;
-import com.alok.repoph.pojo.ApiResponse;
-import com.alok.repoph.pojo.AuthenticationRequest;
 import com.alok.repoph.services.UtilityUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -27,57 +20,12 @@ public class UtilityUserController {
     UtilityUserService utilityUserService;
 
 
-
-    @PostMapping("/login")
-    public ResponseEntity<?> createAuthenticationToken(@RequestBody AuthenticationRequest authenticationRequest) {
-        LOGGER.info(">>>>> Entering login controller");
-        return utilityUserService.generateAuthenticationToken(authenticationRequest);
-
-    }
-
-    @GetMapping("/register")
-    public String loadRegisterForm() {
-        return "register";
-    }
-
-    @PostMapping("/register")
-    public ResponseEntity<?> registerEndUser(@ModelAttribute("userForm") UtilityUser utilityUser, @RequestParam( defaultValue = "false", required = false, name ="serviceOrEnd") Boolean serviceOrEnd ) {
-        LOGGER.info(">>>>>Entering into registerController");
-//        System.out.println(utilityUser.getEmail()+" "+utilityUser.getFullName()+ " "+serviceOrEnd);
-        if ((serviceOrEnd)) {
-            utilityUser.setRole("service");
-        } else {
-            utilityUser.setRole("end");
-        }
-        try {
-            ApiResponse apiResponse = utilityUserService.registerUtilityUser(utilityUser);
-            LOGGER.info("<<<<<Exiting from registerController");
-            return CommonHelperFunctions.buildResponseEntity(apiResponse);
-        } catch (Exception e) {
-            LOGGER.info("<<<<<Exiting from registerController");
-            return CommonHelperFunctions.buildResponseEntity(new ApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, Constants.INTERNAL_SERVER_ERROR_MESSAGE));
-        }
-
-    }
-
     @GetMapping("/login")
-    public String loadLogInForm(Model model) {
-        model.addAttribute("authentication",new AuthenticationRequest());
-        return "login";
-    }
-
-    @GetMapping("/getMyProfile")
-    public ResponseEntity<?> getProfile(Principal principal) {
-        LOGGER.info(">>>>>Entering into getProfile");
-        try {
-            UtilityUser utilityUser = utilityUserService.findByEmail(principal.getName());
-            LOGGER.info("<<<<<Exiting from getProfile");
-            return CommonHelperFunctions.buildResponseEntity(new ApiResponse(HttpStatus.OK,Constants.SUCCESS_MESSAGE,utilityUser));
-        } catch (Exception e) {
-            LOGGER.info("<<<<<Exiting from getProfile");
-            return CommonHelperFunctions.buildResponseEntity(new ApiResponse(HttpStatus.INTERNAL_SERVER_ERROR, Constants.INTERNAL_SERVER_ERROR_MESSAGE));
+    public String loadLogInForm(Principal principal) {
+        if (principal != null) {
+            return "redirect:/";
+        } else {
+            return "login";
         }
     }
-
-
 }
