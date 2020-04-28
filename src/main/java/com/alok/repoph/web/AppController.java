922d01d55f4a85a62661dac5f1aa14bae9931b54
alok-pol <ravikantpal010@gmail.com>
2020-04-28 14:38:57 +0530

    cart is working well

--- src/main/java/com/alok/repoph/web/AppController.java
+++ src/main/java/com/alok/repoph/web/AppController.java
@@ -2,16 +2,17 @@ package com.alok.repoph.web;
 
 import com.alok.repoph.models.User;
 import com.alok.repoph.services.AppService;
+import com.alok.repoph.services.UserServiceImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.security.Principal;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -20,6 +21,9 @@ public class AppController {
 
     @Autowired
     AppService appService;
+
+    @Autowired
+    UserServiceImpl userService;
 
     @GetMapping("/home")
     public String loadHomePage(Model model) {
@@ -42,6 +46,33 @@ public class AppController {
         }
         model.addAttribute("publicProfile",user);
         return "public-profile";
+    }
+
+
+    @PostMapping("/add-to-cart")
+    public String addToCart(
+            @RequestParam("id") String ids, Model model) {
+
+        List<User> userTobeHire = new ArrayList<>();
+        String[] usersIdsList = ids.split(",");
+        Arrays.asList(usersIdsList).forEach(id -> {
+            userTobeHire.add(appService.getUserById(Long.parseLong(id)));
+        });
+        model.addAttribute("userTobeHire",userTobeHire);
+        return "ask-hours";
+    }
+
+
+    @PostMapping("/place-order")
+    public String placeOrder(
+            @RequestParam("hours") String hours,
+            @RequestParam("ids") String ids,
+            Model model) {
+
+        System.out.println(hours);
+        System.out.println(ids);
+
+        return "ask-hours";
     }
 
 }
